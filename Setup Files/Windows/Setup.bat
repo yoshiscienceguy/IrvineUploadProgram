0eca889416555b5fae544c31b7b1f4997745d541
Fernando de Paz <fdepaz@uci.edu>
2015-12-13 11:08:15 -0800

    Python is now silent!
    the program doesn't open a console.

--- Setup Files/Windows/Setup.bat
+++ Setup Files/Windows/Setup.bat
@@ -8,7 +8,7 @@ set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = "%USERPROFILE%\Desktop\MxUpload.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = "C:\Users\%USERNAME%\Documents\IrvineUploadProgram\src\MxPiDrive\GUI.py" >> %SCRIPT%
+echo oLink.TargetPath = "C:\Users\%USERNAME%\Documents\IrvineUploadProgram\src\MxPiDrive\GUI.pyw" >> %SCRIPT%
 echo oLink.IconLocation = "C:\Users\%USERNAME%\Documents\IrvineUploadProgram\src\logo.ico" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 
